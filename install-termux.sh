072cc3f9ef07815b7f7b00b2ebef7b426858076d
Wolffyhtl <matextsanzhedie@outlook.com>
2025-09-15 19:34:55 +0800

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,8 +1,6 @@
 pkg update && pkg upgrade -y
 pkg install proot-distro -y
 pd i ubuntu
-cp 
+cp install-termux-proot-arm64.sh data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu
+cp start-termux-proot-arm64.sh data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu
 pd sh ubuntu
-
-
-
