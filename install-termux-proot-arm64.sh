658922cab32cbd334598410c4fa216a7e549af50
Wolffyhtl <matextsanzhedie@outlook.com>
2025-09-15 20:21:21 +0800

    Update install-termux-proot-arm64.sh

--- install-termux-proot-arm64.sh
+++ install-termux-proot-arm64.sh
@@ -7,7 +7,7 @@ DOTNET_FILE=dotnet-sdk-7.0.203-linux-arm64.tar.gz
 export DOTNET_ROOT=/root/.dotnet
 mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
-echo -e "Next, you need to press the PageDown button and paste the following text into a new line.\nYou will paste this:\n\033[31mexport DOTNET_ROOT=/root/.dotnet/\033[0m\n\033[31mexport PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools\033[0m\nNext, save the file by pressing \033[1;33mCtrl+X\033[0m, typing \033[1;33mY\033[0m and then pressing \033[1;33mEnter\033[0m."
+echo -e "Next, you need to press the PageDown button and paste the following text into a new line.\nYou will paste this:\n\033[31mexport DOTNET_ROOT=/root/.dotnet/\033[0m\n\033[31mexport PATH=\$PATH:\$DOTNET_ROOT:\$DOTNET_ROOT/tools\033[0m\nNext, save the file by pressing \033[1;33mCtrl+X\033[0m, typing \033[1;33mY\033[0m and then pressing \033[1;33mEnter\033[0m."
 nano /root/.bashrc
 dotnet
 cd /
